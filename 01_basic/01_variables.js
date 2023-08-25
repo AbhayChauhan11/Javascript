7dba42874de4a7ceeb52490f854f526a81df3bf3
Abhay Chuahan <abhaychauhan.2018616@gmail.com>
2023-08-25 19:24:42 +0000

    1_lecture

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const account_id=101001
+let accountEmail="abychauhan12@gamil.com"
+var accountPasswd="12345"
+accountCity="Dehradun"
+let accountState;
+/*
+prefer not to use because
+of issue of block scope and issue scope
+*/
+// account_id=2 //not allowed
+accountEmail="asf123@google.com"
+accountPasswd="123213"
+accountCity="Bageshwar"
+console.log(account_id)
+console.table([account_id,accountEmail,accountPasswd,accountCity,accountState])
