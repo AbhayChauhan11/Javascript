a52943a071899374192689dd8aac9fa4e83d1386
Abhay Chuahan <abhaychauhan.2018616@gmail.com>
2023-08-28 18:00:59 +0000

    discussions

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,7 +2,7 @@ const account_id=101001
 let accountEmail="abychauhan12@gamil.com"
 var accountPasswd="12345"
 accountCity="Dehradun"
-let accountState;
+let accountState=null;
 /*
 prefer not to use because
 of issue of block scope and issue scope
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";// treat all the js code to new version
+
+//alert(3+3)// error we are using node.js not browser
+
+console.log(typeof null)//its type is object
+console.log(typeof undefined)// its type is undefined
+console.log(typeof "Abhay");
