81699e0f4b4476a89256f19fa3051b30cbac5fb1
Abhay Chuahan <abhaychauhan.2018616@gmail.com>
2023-08-29 04:57:54 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,22 @@
+let gameName="Abhay"
+// let gameNamee=gameName
+// gameNamee="Abhay Chauhan"
+
+//  console.log(gameName[0]);
+// console.log(gameNamee);
+// console.log(typeof gameNamee);
+// console.log(typeof gameNamee);
+
+// const name="Abhay"
+// const age=21
+// console.log(`hello my name is ${name} and my age is ${age}`);
+// const game=new String("Abhay")
+// let gg=game;
+// gg="manu";
+// console.log(typeof game);
+// console.log( game.toUpperCase());
+// console.log( game);
+// console.log( game.__proto__);
+// console.log( game.length);
+// console.log(typeof gg);
+// console.log(gg);
