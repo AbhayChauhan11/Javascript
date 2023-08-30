c6573750bb5185418e0e794c997530051072d9ec
Abhay Chuahan <abhaychauhan.2018616@gmail.com>
2023-08-30 11:17:43 +0000

    done with math and number

---
+++ 01_basic/nums_and_maths.js
@@ -0,0 +1,24 @@
+// const score=400
+// console.log(score);
+
+// const balance =new Number(400);
+// console.log(balance);
+// let b=balance
+// b=500
+// console.log(b);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// // mostly used in commerce website
+
+// const otherNumber=1233.4412;
+// console.log(otherNumber.toPrecision(4));
+// // set the precision to number.
+
+// const number=1000000;
+// console.log(number.toLocaleString('en-IN'));
+// it help us to visualize the value in better format
+// 'en-In' to mention it in India format by default in US.
+
+//****************Maths****************** */
+
+console.log(Math);
